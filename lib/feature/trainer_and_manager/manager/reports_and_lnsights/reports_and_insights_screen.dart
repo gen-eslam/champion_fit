937becc903da43a74bd1779c593db810314ef267
gen-eslam <romancawy12@gmail.com>
2024-04-24 20:27:14 +0200

    manager finsh

--- lib/feature/trainer_and_manager/manager/reports_and_lnsights/reports_and_insights_screen.dart
+++ lib/feature/trainer_and_manager/manager/reports_and_lnsights/reports_and_insights_screen.dart
@@ -5,6 +5,9 @@ import 'package:gem_app2/core/theme/manager/colors_manager.dart';
 import 'package:gem_app2/core/theme/manager/text_style_manager.dart';
 import 'package:gem_app2/core/utils/space_Manager.dart';
 import 'package:gem_app2/core/widgets/custom_text.dart';
+import 'package:gem_app2/firebase/firebase_firestore_service.dart';
+import 'package:gem_app2/firebase/tables_name.dart';
+import 'package:gem_app2/models/user_model.dart';
 
 class ReportsAndInsightsScreen extends StatelessWidget {
   const ReportsAndInsightsScreen({super.key});
@@ -18,27 +21,48 @@ class ReportsAndInsightsScreen extends StatelessWidget {
           style: TextStyleManager.textStyle30w700,
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 10.r,
-          horizontal: 25.r,
-        ),
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              const ReportAndInsightsItem(text: "Total Memberships: 500"),
-              AppSizedBox.h32,
-              const ReportAndInsightsItem(text: "Total Trainers: 30"),
-              AppSizedBox.h32,
-              const ReportAndInsightsItem(text: r"Total Sales: 250000$"),
-              AppSizedBox.h32,
-              const ReportAndInsightsItem(text: "Active members: 200"),
-            ],
-          ),
-        ),
+      body: FutureBuilder(
+        future: FirebaseFireStoreService.getDocsData(
+            tableName: TablesName.users, fromJson: UserModel.fromJson),
+        builder: (context, snapshot) {
+          return Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 10.r,
+              horizontal: 25.r,
+            ),
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  ReportAndInsightsItem(
+                      text: "Total Memberships: ${snapshot.data?.length}"),
+                  AppSizedBox.h32,
+                  ReportAndInsightsItem(
+                      text:
+                          "Total Trainers:${snapshot.data?.where((element) => element.role == "trainer").length}"),
+                  AppSizedBox.h32,
+                  ReportAndInsightsItem(
+                      text:
+                          "Total Sales: ${getAllSales(userModel: snapshot.data ?? [])}\$"),
+                  AppSizedBox.h32,
+                  ReportAndInsightsItem(
+                      text:
+                          "Active members:${snapshot.data?.where((element) => element.role == "customer").length}"),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
+  }
+
+  double getAllSales({required List<UserModel> userModel}) {
+    double total = 0;
+    for (var element in userModel) {
+      total += element.subscription!;
+    }
+    return total;
   }
 }
 
